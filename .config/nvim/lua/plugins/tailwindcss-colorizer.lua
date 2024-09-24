4a34f266e4f632226d3ee050dc7f19675031d8ff
Pachwenko <patrick.mccartney@imtapps.com>
2024-09-24 07:54:39 -0700

    Peripheral: add tailwindcss colorizer

---
+++ .config/nvim/lua/plugins/tailwindcss-colorizer.lua
@@ -0,0 +1,10 @@
+-- https://github.com/roobert/tailwindcss-colorizer-cmp.nvim
+return {
+	"roobert/tailwindcss-colorizer-cmp.nvim",
+	-- optionally, override the default options:
+	config = function()
+		require("tailwindcss-colorizer-cmp").setup({
+			color_square_width = 2,
+		})
+	end,
+}
