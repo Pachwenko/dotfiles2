63cdbbf7a1d54fff2834995eba791b5ab5d2a7a2
Pachwenko <patrick.mccartney@imtapps.com>
2024-09-25 07:38:19 -0700

    Peripheral: organize dotfiles more

---
+++ .config/nvim/lua/custom_plugins/settings.lua
@@ -0,0 +1,90 @@
+-- Add a line length column
+vim.g.colorcolumn = 120
+
+-- See official kicksart video here
+-- https://www.youtube.com/watch?v=m8C0Cq9Uv9o
+-- Disable GUI cursor
+vim.opt.guicursor = ""
+
+-- Line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- Search settings
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Other options
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 4
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+vim.opt.mouse = "a"
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
+vim.opt.conceallevel = 2
+
+-- some configs from Kickstart project
+vim.g.have_nerd_font = false
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
