9c685c29481e23f6285c45a2107267498ec50917
Pachwenko <patrick.mccartney@imtapps.com>
2024-11-02 12:13:52 -0700

    Peripheral: add basedpyright

--- patricks_neovim/.config/patricks_neovim/lua/plugins/nvim-lspconfig.lua
+++ patricks_neovim/.config/patricks_neovim/lua/plugins/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = {
 		-- Automatically install LSPs and related tools to stdpath for Neovim
 		{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants "williamboman/mason-lspconfig.nvim",
-    "williamboman/mason-lspconfig.nvim",
+		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		-- Useful status updates for LSP.
@@ -92,7 +92,7 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-        map("<leader>fm", vim.lsp.buf.format, "[F]or[m]at file with current LSP")
+				map("<leader>fm", vim.lsp.buf.format, "[F]or[m]at file with current LSP")
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
@@ -152,30 +152,33 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			-- clangd = {},
-			-- gopls = {},
-			pyright = {
-				capabilities = capabilities,
-				on_attach = function(client, bufnr)
-					-- 'Organize imports' keymap for pyright only
-					vim.keymap.set("n", "<Leader>ii", "<cmd>PyrightOrganizeImports<CR>", {
-						buffer = bufnr,
-						silent = true,
-						noremap = true,
-					})
-				end,
-				-- See  https://www.andersevenrud.net/neovim.github.io/lsp/configurations/pyright/
+			-- rust_analyzer = {},
+			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+			--
+			-- Some languages (like typescript) have entire language plugins that can be useful:
+			--    https://github.com/pmizio/typescript-tools.nvim
+			--
+			-- But for many setups, the LSP (`ts_ls`) will work just fine
+			--
+			ts_ls = {},
+			basedpyright = {
+				-- https://docs.basedpyright.com/latest/
+				-- better than pyright for reasons - see website
+				--
+				-- See https://docs.basedpyright.com/latest/configuration/language-server-settings/
 				-- for more config options
+				capabilities = capabilities,
 				settings = {
-					single_file_support = true,
-					python = {
-						disableOrganizeImports = false,
+					basedpyright = {
+						disableOrganizeImports = true,
 						analysis = {
-							autoSearchPaths = false,
-							typeCheckingMode = "off", -- Disable type checking completely
+							typeCheckingMode = "off", -- options are ["off", "basic", "standard", "strict", "recommended", "all"]
 							diagnosticMode = "openFilesOnly", -- deafult is "workspace" but "openFilesOnly" can be used to limit diagnostics
+							autoSearchPaths = false,
 							autoImportCompletions = true,
 							useLibraryCodeForTypes = true,
+						},
+						python = {
 							venvPath = "~/.pyenv/versions/",
 						},
 					},
@@ -195,22 +198,22 @@ return {
 					},
 				},
 			},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			ts_ls = {},
 			--
 			--
 			ruff = {
-				-- organize imports disabled, since we are already using `isort` for that
-				-- alternative, this can be enabled to make `organize imports`
-				-- available as code action
 				settings = {
-					organizeImports = false,
+					pyright = {
+						-- Using Ruff's import organizer instead of pyright
+						disableOrganizeImports = true,
+					},
+					basedpyright = {
+						-- Using Ruff's import organizer instead of pyright
+						disableOrganizeImports = true,
+					},
+					-- organizeImports:
+					--  this can be enabled to make `organize imports`
+					--  available as code action
+					organizeImports = true,
 				},
 				-- disable ruff as hover provider to avoid conflicts with pyright
 				on_attach = function(client)
@@ -219,16 +222,11 @@ return {
 			},
 
 			lua_ls = {
-				-- cmd = {...},
-				-- filetypes = { ...},
-				-- capabilities = {},
 				settings = {
 					Lua = {
 						completion = {
 							callSnippet = "Replace",
 						},
-						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-						-- diagnostics = { disable = { 'missing-fields' } },
 					},
 				},
 			},
@@ -259,10 +257,10 @@ return {
 			"docker-compose-language-service",
 			"dockerfile-language-server",
 			-- python tools
-			"ruff",  -- ruff replaces a few tools like isort, install with "brew install ruff"
+			"ruff", -- ruff replaces a few tools like isort, install with "brew install ruff"
 			-- "isort",
 			-- "black",
-			"pyright",
+			"basedpyright",
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
